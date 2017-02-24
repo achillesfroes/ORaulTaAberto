1e42a111d150534dc36f009c38d3206181dbb269
Achilles Fróes <achilles.froes@oulook.com>
2017-02-24 16:37:59 -0300

    SignalR, WebSocket, LocalStorage
    Agora é possível fazer checkin, checkout, ver a lista das pessoas que
    chegaram, quando chegaram, a lista atualiza em tempo real. Pode fechar e
    abrir o app que ele mantém o estado.

---
+++ WebApplication4/Hubs/PessoasIO.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using WebApplication4.Models;
+using Newtonsoft.Json;
+
+namespace WebApplication4.Hubs
+{
+    public class PessoasIO : Hub
+    {
+        private static List<Pessoa> pessoasNoRaul;
+
+        public PessoasIO()
+        {
+            if (pessoasNoRaul == null)
+            {
+                pessoasNoRaul = new List<Pessoa>();
+
+                pessoasNoRaul.Add(new Pessoa { Nome = "Zé", QuandoChegou = DateTime.Now.ToString("dd/MM/yyyy hh:mm")});
+            }
+        }
+
+        public void CheckIn(string nick, string dataHora)
+        {
+            if (!pessoasNoRaul.Any(p => p.Nome.Equals(nick)))
+            {
+                AdicionarPessoa(nick, dataHora);
+
+                ObterPessoasNoRaul();
+            }
+        }
+
+        public void CheckOut(string nick)
+        {
+            if (!String.IsNullOrWhiteSpace(nick))
+            {
+                Pessoa pessoa = pessoasNoRaul.FirstOrDefault(p => p.Nome.Equals(nick));
+                pessoasNoRaul.Remove(pessoa);
+
+                ObterPessoasNoRaul();
+            }
+        }
+
+        public void ObterPessoasNoRaul()
+        {
+            Clients.All.listarPessoas(JsonConvert.SerializeObject(pessoasNoRaul));
+        }
+
+        private void AdicionarPessoa(string nick, string dataHora)
+        {
+            pessoasNoRaul.Add(new Pessoa { Nome = nick, QuandoChegou = dataHora });
+        }
+    }
+}
---
+++ WebApplication4/Models/Pessoa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication4.Models
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public string QuandoChegou { get; set; }
+    }
+}
